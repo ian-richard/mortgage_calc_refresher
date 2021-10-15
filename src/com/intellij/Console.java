e04f401a18099d842e266b329a3920805a2a5bd6
Ian F <ian.fiddeman@gmail.com>
2021-10-15 23:00:28 +0100

    mortgageReport and MortgageCalculator classes extracted

--- src/com/intellij/Console.java
+++ src/com/intellij/Console.java
@@ -3,12 +3,22 @@ package com.intellij;
 import java.util.Scanner;
 
 public class Console {
+    //private, i.e. for internal use only
+    private static Scanner scanner = new Scanner(System.in);
+
+
+    //method overloading
+    public static double readNumber(String prompt){
+
+        return scanner.nextDouble();
+    }
+
     public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
+
         double value;
         while (true) {
             System.out.print(prompt);
-            value = scanner.nextFloat();
+            value = scanner.nextDouble();
             if (value >= min && value <= max)
                 break;
             System.out.println("Enter a value between " + min + " and " + max);
